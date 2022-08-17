b61e7918d7b595f7801bfd8087fad44f44e9023f
samir <shrsthsamir@gmail.com>
2022-08-17 14:45:58 +0545

    My websited added

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer id="footer">
+      <div className="container-fluid">
+        <div className="social-icons mt-4">
+          <strong>This is footer</strong>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
